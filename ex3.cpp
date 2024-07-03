114c20f419cff0f96e075f8b41df1da5410d7446
keisanenkobutsu <132887282+keisanenkobutsu@users.noreply.github.com>
2024-07-03 15:40:12 +0900

    Vi

--- ex3.cpp
+++ ex3.cpp
@@ -9,6 +9,10 @@ int main(){
     cout << "Die 1: " << dice1 << endl;
     cout << "Die 2: " << dice2 << endl;
     cout << "Total value: " << dice1 + dice2 << endl; 
-    
+
+    cout << endl; 
+    if(dice1 >= dice2) cout << "You won" << endl;
+    else cout << "You lost" << endl;
+
     return 0;
 }
